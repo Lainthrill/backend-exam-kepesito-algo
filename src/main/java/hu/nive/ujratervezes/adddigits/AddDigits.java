d6c0f78d8750903db31f0b6a4b9912649114ab54
Lainthrill <adyyadyy@gmail.com>
2021-04-29 14:23:51 +0200

    utólagos commit

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,17 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    public int addDigits(String input) {
+        int result = 0;
+        if (input == null || input.length() == 0) {
+            result = -1;
+            return result;
+        }
+        for (int i = 0; i < input.length(); i++) {
+            if (Character.isDigit(input.charAt(i))){
+                result += Character.getNumericValue(input.charAt(i));
+            }
+        }
+        return result;
+    }
 }
